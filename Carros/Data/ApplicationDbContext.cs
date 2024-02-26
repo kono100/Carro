916db77577e52d8fcd739a3d006cb9fefb107e51
Thiago Campos <thiago-kono@hotmail.com>
2024-02-26 20:26:21 -0300

    add

---
+++ Carros/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Carros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Carros.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+            {
+            }
+
+        public DbSet<Car> Cars {  get; set; }
+    }
+}
+    
---
+++ Carros/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Carros.Models
+{
+    public class Car
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cor { get; set; }
+    }
+}
+    
